d3b18aa793f41c30090c0d40c393c5b9153c1e0c
netocalegari <netokalegari@gmail.com>
2024-07-01 10:18:47 -0300

    Stitches styling lib, header

---
+++ src/styles/index.ts
@@ -0,0 +1,26 @@
+import { createStitches } from "@stitches/react";
+
+export const {
+  config,
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+} = createStitches({
+  theme: {
+    colors: {
+      white: "#FFF",
+
+      gray900: "#121214",
+      gray800: "#202024",
+      gray300: "#C4C4CC",
+      gray100: "#E1E1E6",
+
+      green500: "#00875f",
+      green300: "#00b37e",
+    },
+  },
+});
