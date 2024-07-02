1aef153aecf04bce14d6902c0dd6415dabdf8422
netocalegari <netokalegari@gmail.com>
2024-07-02 10:09:36 -0300

    Initiating product details

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,32 @@
+import Image from "next/image";
 import { useRouter } from "next/router";
+
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "@/styles/pages/product";
 
 export default function Product() {
   const { query } = useRouter();
-  return <h1>Product: {JSON.stringify(query)}</h1>;
+
+  return (
+    <ProductContainer>
+      <ImageContainer>{/* <Image/> */}</ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+          repellendus, deserunt dolor sed totam soluta, laboriosam aut
+          necessitatibus quam id excepturi earum aliquid nam dolorum, mollitia
+          modi quisquam nostrum veritatis.
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
