eae77c3f21daaee37aef860936ce54257ab81de4
netocalegari <netokalegari@gmail.com>
2024-07-03 09:27:12 -0300

    Payment, success page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,8 @@
+import axios from "axios";
 import { GetStaticPaths, GetStaticProps } from "next";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { useState } from "react";
 import Stripe from "stripe";
 
 import { stripe } from "@/lib/stripe";
@@ -17,10 +19,31 @@ interface ProductProps {
     imageUrl: string;
     price: string;
     description: string;
+    defaultPriceId: string;
   };
 }
 export default function Product({ product }: ProductProps) {
   const { isFallback } = useRouter();
+  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] =
+    useState(false);
+
+  const handleBuyProduct = async () => {
+    try {
+      setIsCreatingCheckoutSession(true);
+
+      const response = await axios.post("/api/checkout", {
+        priceId: product.defaultPriceId,
+      });
+
+      const { checkoutUrl } = response.data;
+
+      window.location.href = checkoutUrl;
+    } catch (err) {
+      setIsCreatingCheckoutSession(false);
+
+      alert("Falha ao redirecionar ao checkout!");
+    }
+  };
 
   if (isFallback) {
     return <p>Loading...</p>;
@@ -37,7 +60,9 @@ export default function Product({ product }: ProductProps) {
 
         <p>{product.description}</p>
 
-        <button>Comprar agora</button>
+        <button disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>
+          Comprar agora
+        </button>
       </ProductDetails>
     </ProductContainer>
   );
@@ -70,6 +95,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
           currency: "BRL",
         }).format(price.unit_amount! / 100),
         description: product.description,
+        defaultPriceId: price.id,
       },
     },
     revalidate: 60 * 60 * 1, // 1 hour
--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,22 @@
+import Link from "next/link";
+
+import { ImageContainer, SuccessContainer } from "@/styles/pages/success";
+
 export default function Success() {
-  return <h1>Success</h1>;
+  return (
+    <SuccessContainer>
+      <h1>Compra Efetuada!</h1>
+
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Sua camiseta <strong>XXXXXXXXX</strong> já está sendo preparada e será
+        enviada para seu endereço.
+      </p>
+
+      <Link href={`/`} passHref legacyBehavior>
+        voltar ao catálogo
+      </Link>
+    </SuccessContainer>
+  );
 }
